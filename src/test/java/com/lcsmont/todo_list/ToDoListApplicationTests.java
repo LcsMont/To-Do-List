3ffe14e9c04bf49c4634664d1fdc2071159aaf0f
Lucas Monteiro <lucasmmnm@gmail.com>
2025-01-01 10:33:01 -0300

    Project Setup

---
+++ src/test/java/com/lcsmont/todo_list/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcsmont.todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
